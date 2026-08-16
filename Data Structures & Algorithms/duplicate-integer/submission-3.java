7344fd0ac6bc739a7b10b1ff19f21befe0a565c0
dwivediansh598-hue <dwivediansh598@gmail.com>
2026-08-16 23:29:14 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int n=nums.length;
+       Arrays.sort(nums);
+       int i=0;int j=1;
+       while(j<n){
+        if(nums[j]==nums[i]){
+            return true;
+        }
+        i++;
+        j++;
+       }
+       return false;
+    }
+}
